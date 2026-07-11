d11b80ea7a88baa70d5c8cd01c868e0736020ca0
Ananthu <122630777+Ananthux2004@users.noreply.github.com>
2026-07-11 14:24:19 +0530

    project cleanup

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files (user uploads)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Static files (collected for deployment)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
