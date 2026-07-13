4b0bbd151febadf95f37f3f7b35ac2eff013895f
saswathkm <saswathkm19105@gmail.com>
2026-07-14 00:10:41 +0530

    added comment functions

--- blogproject/blog/forms.py
+++ blogproject/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-
-from .models import Post
+# Added Comment to the import below
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -29,3 +29,29 @@ class PostForm(forms.ModelForm):
             "status": forms.Select(attrs={"class": "form-select"}),
             "is_featured": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
+
+# --- NEW COMMENT FORM ADDED BELOW ---
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        # We only want the user to type the content itself
+        fields = ['content'] 
+        
+        # Adding Bootstrap classes for styling and validation constraints
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'rows': 3, 
+                'placeholder': 'Write a comment...',
+                'minlength': '2',
+                'maxlength': '1000'
+            }),
+        }
+        
+    def clean_content(self):
+        """Strips whitespace from the beginning and end of the comment"""
+        content = self.cleaned_data.get('content')
+        if content:
+            return content.strip()
+        return content
