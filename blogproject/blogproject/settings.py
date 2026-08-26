432e609437ac10a3516751be4e2cd5d9be3c93f3
Ananthux2004 <anandhu8839@gmail.com>
2026-08-26 10:26:43 -0700

    Fix WhiteNoise MissingFileError during collectstatic

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -114,6 +114,8 @@ STORAGES = {
     },
 }
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False  
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
